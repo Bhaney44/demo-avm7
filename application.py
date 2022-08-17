81f99f616ba8a1bd983576d2a2be42b67581a296
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-17 06:55:15 -0400

    Adding jsonref example

--- application.py
+++ application.py
@@ -3,43 +3,59 @@ from typing import Literal
 from pyteal import *
 from beaker import *
 
+
 class DemoAVM7(Application):
     @external
-    def vrf_verify(self, msg: abi.DynamicArray[abi.Byte], proof: abi.StaticArray[abi.Byte, Literal[80]]):
+    def vrf_verify(
+        self,
+        msg: abi.DynamicArray[abi.Byte],
+        proof: abi.StaticArray[abi.Byte, Literal[80]],
+    ):
         return Approve()
 
     @external
     def replace(self):
-        #replace2            
-        #replace3            
+        # replace2
+        # replace3
         return Approve()
 
     @external
     def b64decode(self):
-        #base64_decode       
+        # base64_decode
         return Approve()
 
+    JsonExampleResult = abi.Tuple3[abi.String, abi.Uint64, abi.String]
+
     @external
-    def json_ref(self):
-        #json_ref            
-        return Approve()
+    def json_ref(self, json_str: abi.String, *, output: JsonExampleResult):
+        return Seq(
+            (s := abi.String()).set(
+                JsonRef.as_string(json_str.get(), Bytes("string_key"))
+            ),
+            (i := abi.Uint64()).set(
+                JsonRef.as_uint64(json_str.get(), Bytes("uint_key"))
+            ),
+            (o := abi.String()).set(
+                JsonRef.as_object(json_str.get(), Bytes("obj_key"))
+            ),
+            output.set(s, i, o),
+        )
 
     @external
     def ed25519verify_bare(self):
-        #ed25519verify_bare  
+        # ed25519verify_bare
         return Approve()
 
     @external
     def sha3_256(self):
-        #sha3_256            
+        # sha3_256
         return Approve()
 
     @external
     def block(self):
         # seed
-        #block  (Seed and timestamp)
+        # block  (Seed and timestamp)
         return Approve()
-
 
     @delete(authorize=Authorize.only(Global.creator_address()))
     def delete(self):
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from application import DemoAVM7
 from beaker import client, sandbox
 
+
 def demo():
 
     app_client = client.ApplicationClient(
         client=sandbox.get_algod_client(),
         app=DemoAVM7(),
-        signer=sandbox.get_accounts().pop().signer
+        signer=sandbox.get_accounts().pop().signer,
     )
 
     app_id, app_addr, _ = app_client.create()
     print(f"Created app with id {app_id} and address {app_addr}")
 
+    call_json_ref(app_client)
     # app_client.call(DemoAVM7.vrf_verify, ... )
     # app_client.call(DemoAVM7.replace, ...)
     # app_client.call(DemoAVM7.b64decode, ...)
@@ -22,6 +24,26 @@ def demo():
 
     print("deleting app")
     app_client.delete()
+
+
+def call_json_ref(app_client: client.ApplicationClient):
+    import json
+
+    obj = {
+        "string_key": "In Xanadu did Kubla Khan",
+        "uint_key": 42,
+        "obj_key": {"lol": "lmao"},
+    }
+
+    json_str = json.dumps(obj)
+    result = app_client.call(DemoAVM7.json_ref, json_str=json_str)
+    print(f"result {result.return_value}")
+
+    string_key_value, uint_key_value, obj_key_value = result.return_value
+    assert obj["string_key"] == string_key_value
+    assert obj["uint_key"] == uint_key_value
+    assert json.dumps(obj["obj_key"]) == obj_key_value
+
 
 if __name__ == "__main__":
     demo()
